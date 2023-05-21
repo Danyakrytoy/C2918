1d98c98079b9424e97cc41245fd4b0911056c12d
Danyakrytoy <dan.ushakoff@gmail.com>
2023-05-21 11:39:50 +0300

    dz_super

--- main.py
+++ main.py
@@ -21,8 +21,20 @@ class Display:
         super().__init__()
         self.resolution = "4k"
 
-class SmartPhone(Display, Computer):
+class Laptop:
+    def __init__(self):
+        super().__init__()
+        self.processor = "Intel Core i9‑12900HK (Alder Lake‑P)"
+
+class Size_screen:
+    def __init__(self):
+        super().__init__()
+        self.size = "(6.06 inches)"
+
+class SmartPhone(Display, Computer, Laptop, Size_screen):
     def print_into(self):
+        print(self.size)
+        print(self.processor)
         print(self.resolution)
         print(self.memory)
 iphone = SmartPhone()
