32240b7160ec4d13050ee48f35b6609ad859b0da
Danyakrytoy <dan.ushakoff@gmail.com>
2023-06-10 19:49:12 +0300

    wdawdasddz

---
+++ main.py
@@ -0,0 +1,29 @@
+import urllib.request
+import requests
+
+a = int(input("Гривні у Долари: "))
+# opener = urllib.request.build_opener()
+uah = a / 37
+# response = opener.open("https://httpbin.org/get")
+#
+# print(response.read())
+# res = requests.post("https://httpbin.org/get", data="Test data hello world", headers={"h1": "My tittle"})
+# print(res.text)
+# response = requests.get("https://httpbin.org/get")
+# print(response.text)
+
+
+
+response = requests.get("https://coinmarketcap.com/")
+response_text = response.text
+print(response.text)
+
+valuta_list = []
+response_parse = response_text.split("<span>")
+for parse_elem1 in response_parse:
+    if parse_elem1.startswith("$"):
+        for parse_elem2 in parse_elem1.split("</span>"):
+            if parse_elem2.startswith("$") and parse_elem2[1].isdigit():
+                valuta_list.append(parse_elem2)
+uah1 = valuta_list[5]
+print("USD =", uah, "гривні")
